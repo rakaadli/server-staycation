ced179b515bc9d82d79b34eaabda09bfcfe31868
rakaadli <34180704+rakaadli@users.noreply.github.com>
2020-05-10 05:31:16 +0700

    fixed api landing page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -12,7 +12,10 @@ module.exports = {
       const mostPicked = await Item.find()
         .select('_id title country city price unit imageId')
         .limit(5)
-        .populate({ path: 'imageId', select: '_id imageUrl' })
+        .populate({
+          path: 'imageId',
+          select: '_id imageUrl'
+        })
 
       const category = await Category.find()
         .select('_id name')
@@ -21,7 +24,11 @@ module.exports = {
           path: 'itemId',
           select: '_id title country city isPopular  imageId',
           perDocumentLimit: 4,
-          option: { sort: { sumBooking: -1 } },
+          option: {
+            sort: {
+              sumBooking: -1
+            }
+          },
           populate: {
             path: 'imageId',
             select: '_id imageUrl',
@@ -35,7 +42,9 @@ module.exports = {
 
       for (let i = 0; i < category.length; i++) {
         for (let x = 0; x < category[i].itemId.length; x++) {
-          const item = await Item.findOne({ _id: category[i].itemId[x]._id });
+          const item = await Item.findOne({
+            _id: category[i].itemId[x]._id
+          });
           item.isPopular = false;
           await item.save();
           if (category[i].itemId[0] === category[i].itemId[x]) {
@@ -67,17 +76,32 @@ module.exports = {
       })
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({
+        message: "Internal server error"
+      });
     }
   },
 
   detailPage: async (req, res) => {
     try {
-      const { id } = req.params;
-      const item = await Item.findOne({ _id: id })
-        .populate({ path: 'featureId', select: '_id name qty imageUrl' })
-        .populate({ path: 'activityId', select: '_id name type imageUrl' })
-        .populate({ path: 'imageId', select: '_id imageUrl' });
+      const {
+        id
+      } = req.params;
+      const item = await Item.findOne({
+          _id: id
+        })
+        .populate({
+          path: 'featureId',
+          select: '_id name qty imageUrl'
+        })
+        .populate({
+          path: 'activityId',
+          select: '_id name type imageUrl'
+        })
+        .populate({
+          path: 'imageId',
+          select: '_id imageUrl'
+        });
 
       const bank = await Bank.find();
 
@@ -98,7 +122,9 @@ module.exports = {
       })
 
     } catch (error) {
-      res.status(500).json({ message: "Internal server error" });
+      res.status(500).json({
+        message: "Internal server error"
+      });
     }
   },
 
@@ -118,7 +144,9 @@ module.exports = {
     } = req.body;
 
     if (!req.file) {
-      return res.status(404).json({ message: "Image not found" });
+      return res.status(404).json({
+        message: "Image not found"
+      });
     }
 
     console.log(idItem)
@@ -135,13 +163,19 @@ module.exports = {
       phoneNumber === undefined ||
       accountHolder === undefined ||
       bankFrom === undefined) {
-      res.status(404).json({ message: "Lengkapi semua field" });
+      res.status(404).json({
+        message: "Lengkapi semua field"
+      });
     }
 
-    const item = await Item.findOne({ _id: idItem });
+    const item = await Item.findOne({
+      _id: idItem
+    });
 
     if (!item) {
-      return res.status(404).json({ message: "Item not found" });
+      return res.status(404).json({
+        message: "Item not found"
+      });
     }
 
     item.sumBooking += 1;
@@ -182,6 +216,9 @@ module.exports = {
 
     const booking = await Booking.create(newBooking);
 
-    res.status(201).json({ message: "Success Booking", booking });
+    res.status(201).json({
+      message: "Success Booking",
+      booking
+    });
   }
 }
